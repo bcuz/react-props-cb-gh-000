84d5b21c21dcba49efcb0a2524c049b6f915f660
Adam N <adam.nawroc@gmail.com>
2019-01-02 17:25:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ class MovieCard extends React.Component {
   render() {
     return (
       <div className="movie-card">
-        <img src="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="Mad Max: Fury Road" />
-        <h2>Mad Max: Fury Road</h2>
-        <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
-      </div>
+  <img  
+    src={this.props.poster} 
+    alt={this.props.title} 
+  />
+  <h2>{this.props.title}</h2>
+  <small>Genres: {this.props.genres.join(', ')}</small>
+</div>
     );
   }
 }
