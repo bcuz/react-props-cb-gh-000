ba36fe2bffbcc318e652d899bedf27301b9a5586
Adam N <adam.nawroc@gmail.com>
2019-01-02 17:29:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.defaultProps = {
-  poster: 'http://i.imgur.com/bJw8ndW.png'
+  // poster: 'http://i.imgur.com/bJw8ndW.png'
 };
 
 const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
